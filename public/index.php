939aeae494a8a870cf8af652ddaf50efe6063894
Abraham <20031313@itcelaya.edu.mx>
2026-09-04 20:49:26 -0600

    Agrega rutas v2 para users y productos

--- public/index.php
+++ public/index.php
@@ -42,6 +42,17 @@ $userResourceV2 = new UserResourceV2();
 $routerV2->addRoute('POST', '/login', [$userResourceV2, 'login'], true); // pública
 $routerV2->addRoute('POST', '/logout', [$userResourceV2, 'logout']);     // protegida
 $routerV2->addRoute('GET', '/me', [$userResourceV2, 'me']);              // protegida
+$routerV2->addRoute('GET', '/users', [$userResourceV1, 'index']);
+$routerV2->addRoute('GET', '/users/{id}', [$userResourceV1, 'show']);
+$routerV2->addRoute('POST', '/users', [$userResourceV1, 'store']);
+$routerV2->addRoute('PUT', '/users/{id}', [$userResourceV1, 'update']);
+$routerV2->addRoute('DELETE', '/users/{id}', [$userResourceV1, 'destroy']);
+
+$routerV2->addRoute('GET', '/productos', [$productResource, 'index']);
+$routerV2->addRoute('GET', '/productos/{id}', [$productResource, 'show']);
+$routerV2->addRoute('POST', '/productos', [$productResource, 'store']);
+$routerV2->addRoute('PUT', '/productos/{id}', [$productResource, 'update']);
+$routerV2->addRoute('DELETE', '/productos/{id}', [$productResource, 'destroy']);
 
 // --- Resolver ruta ---
 $result = $routerV1->resolve() ?? $routerV2->resolve();
